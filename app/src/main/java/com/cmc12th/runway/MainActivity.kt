8122e0b9d2085fe4cdd74c42ce0cc4c780ee916a
dlgocks1 <cksgodl1@naver.com>
2023-02-03 15:50:55 +0900

    Initial commit

---
+++ app/src/main/java/com/cmc12th/runway/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.cmc12th.runway
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.cmc12th.runway.ui.theme.RunwayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RunwayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
+
+
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    RunwayTheme {
+//        Greeting("Android")
+//    }
+//}
