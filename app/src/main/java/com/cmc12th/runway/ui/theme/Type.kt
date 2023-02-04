a84dbd4748dd87d6f5efb5252e5dd9a78d85569d
PCBL <cksgodl1@naver.com>
2023-02-04 18:12:09 +0900

    feat : default font 및 color 수정

--- app/src/main/java/com/cmc12th/runway/ui/theme/Type.kt
+++ app/src/main/java/com/cmc12th/runway/ui/theme/Type.kt
@@ -2,33 +2,76 @@ package com.cmc12th.runway.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.cmc12th.runway.R
+
+val SpoqaHansSansNeo = FontFamily(
+    Font(R.font.spoqa_han_sans_neo_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.spoqa_han_sans_neo_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.spoqa_han_sans_neo_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.spoqa_han_sans_neo_light, FontWeight.Light, FontStyle.Normal),
+)
+
+val HeadLine1 = TextStyle(
+    fontFamily = SpoqaHansSansNeo,
+    fontWeight = FontWeight.Bold,
+    fontSize = 28.sp,
+)
+
+val HeadLine2 = TextStyle(
+    fontFamily = SpoqaHansSansNeo,
+    fontWeight = FontWeight.Bold,
+    fontSize = 24.sp,
+)
+
+val HeadLine3 = TextStyle(
+    fontFamily = SpoqaHansSansNeo,
+    fontWeight = FontWeight.Bold,
+    fontSize = 20.sp,
+)
+
+val HeadLine4 = TextStyle(
+    fontFamily = SpoqaHansSansNeo,
+    fontWeight = FontWeight.Bold,
+    fontSize = 18.sp,
+)
+
+val Body1 = TextStyle(
+    fontFamily = SpoqaHansSansNeo,
+    fontWeight = FontWeight.Bold,
+    fontSize = 16.sp,
+)
+
+val Body2 = TextStyle(
+    fontFamily = SpoqaHansSansNeo,
+    fontWeight = FontWeight.Bold,
+    fontSize = 14.sp,
+)
+
+val Caption = TextStyle(
+    fontFamily = SpoqaHansSansNeo,
+    fontWeight = FontWeight.Bold,
+    fontSize = 12.sp,
+)
+
+val Button1 = TextStyle(
+    fontFamily = SpoqaHansSansNeo,
+    fontWeight = FontWeight.Medium,
+    fontSize = 16.sp,
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = SpoqaHansSansNeo,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
+
