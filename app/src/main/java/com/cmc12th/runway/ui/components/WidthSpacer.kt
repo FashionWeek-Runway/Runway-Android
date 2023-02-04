b40c0facd503ad0fcac33dfcb8ac5632d7aa57ac
PCBL <cksgodl1@naver.com>
2023-02-05 02:06:17 +0900

    fix : WidthSpacer은 모든 컴포저블에서 사용가능해야함

--- app/src/main/java/com/cmc12th/runway/ui/components/WidthSpacer.kt
+++ app/src/main/java/com/cmc12th/runway/ui/components/WidthSpacer.kt
@@ -1,6 +1,5 @@
 package com.cmc12th.runway.ui.components
 
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
@@ -8,6 +7,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 
 @Composable
-fun RowScope.WidthSpacer(width: Dp) {
+fun WidthSpacer(width: Dp) {
     Spacer(modifier = Modifier.width(width))
 }
