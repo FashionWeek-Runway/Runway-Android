4b753e3c101b8a0c3f95ef26694147c78fae9514
PCBL <cksgodl1@naver.com>
2023-02-05 17:44:17 +0900

    feat : Splash 화면 넘어가는 기능 구현

--- app/src/main/java/com/cmc12th/runway/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/cmc12th/runway/ui/splash/SplashScreen.kt
@@ -4,20 +4,33 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.cmc12th.runway.R
+import com.cmc12th.runway.ui.domain.model.ApplicationState
+import com.cmc12th.runway.utils.Constants.MAIN_GRAPH
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen() {
-    Box(modifier = Modifier.fillMaxSize()) {
+fun SplashScreen(appState: ApplicationState) {
+
+    LaunchedEffect(key1 = Unit) {
+        delay(300L)
+        appState.navController.navigate(MAIN_GRAPH)
+    }
+    Box(
+        modifier = Modifier
+            .statusBarsPadding()
+            .navigationBarsPadding()
+            .fillMaxSize()
+    ) {
         Image(
             modifier = Modifier
                 .align(Alignment.TopCenter)
@@ -25,26 +38,25 @@ fun SplashScreen() {
                 .fillMaxHeight(0.7f),
             painter = painterResource(id = R.drawable.img_splash_background),
             contentDescription = "SPLAH_BACKGROUND",
-            contentScale = ContentScale.Crop)
+            contentScale = ContentScale.Crop
+        )
         Image(
             modifier = Modifier
                 .align(Alignment.Center)
                 .size(94.dp, 100.dp),
             painter = painterResource(id = R.drawable.img_splash_logo),
-            contentDescription = "SPLAH_LOGO")
+            contentDescription = "SPLAH_LOGO"
+        )
 
-        Text(modifier = Modifier
-            .align(Alignment.BottomCenter)
-            .padding(bottom = 20.dp),
+        Text(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 20.dp),
             text = "RUNWAY",
             fontSize = 40.sp,
-            fontWeight = FontWeight.Bold)
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun SplashScreenPreview() {
-    SplashScreen()
-}
 
