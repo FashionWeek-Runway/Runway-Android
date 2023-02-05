649f98cb7ee707060414fc92632256f06106658d
PCBL <cksgodl1@naver.com>
2023-02-05 18:24:36 +0900

    feat : 네이버지도 클러스터 기능 구현

---
+++ app/src/main/java/com/cmc12th/domain/model/NaverItem.kt
@@ -0,0 +1,36 @@
+package com.cmc12th.domain.model
+
+import com.naver.maps.geometry.LatLng
+import ted.gun0912.clustering.clustering.TedClusterItem
+import ted.gun0912.clustering.geometry.TedLatLng
+
+data class NaverItem(var position: LatLng) : TedClusterItem {
+
+    override fun getTedLatLng(): TedLatLng {
+        return TedLatLng(position.latitude, position.longitude)
+    }
+
+    /**
+     * Set the title of the marker
+     * @param title string to be set as title
+     */
+    var title: String? = null
+
+    /**
+     * Set the description of the marker
+     * @param snippet string to be set as snippet
+     */
+    var snippet: String? = null
+
+    constructor(lat: Double, lng: Double) : this(LatLng(lat, lng)) {
+        title = null
+        snippet = null
+    }
+
+    constructor(lat: Double, lng: Double, title: String?, snippet: String?) : this(
+        LatLng(lat, lng)
+    ) {
+        this.title = title
+        this.snippet = snippet
+    }
+}
--- app/src/main/java/com/cmc12th/runway/ui/map/MapScreen.kt
+++ app/src/main/java/com/cmc12th/runway/ui/map/MapScreen.kt
@@ -1,18 +1,85 @@
+@file:OptIn(ExperimentalNaverMapApi::class)
+
 package com.cmc12th.runway.ui.map
 
+import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import com.cmc12th.domain.model.NaverItem
+import com.cmc12th.runway.R
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.compose.DisposableMapEffect
+import com.naver.maps.map.compose.ExperimentalNaverMapApi
+import com.naver.maps.map.compose.NaverMap
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
+import ted.gun0912.clustering.naver.TedNaverClustering
 
 
+@OptIn(ExperimentalNaverMapApi::class)
 @Composable
 fun MapScreen() {
-    Column(
+
+    val items = listOf<NaverItem>(
+        NaverItem(37.532600, 127.024612),
+        NaverItem(37.390791, 127.096306),
+        NaverItem(37.540791, 127.096306),
+        NaverItem(37.550791, 127.076306),
+        NaverItem(37.560791, 127.066306),
+        NaverItem(37.510791, 127.016306),
+        NaverItem(37.570791, 127.046306),
+    )
+
+    Box(
         modifier = Modifier
             .fillMaxSize()
     ) {
-        Text(text = "Map Screen")
-
+        /** NaverMap Compose
+         *  https://github.com/fornewid/naver-map-compose */
+        NaverMap(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            val context = LocalContext.current
+            var clusterManager by remember { mutableStateOf<TedNaverClustering<NaverItem>?>(null) }
+            DisposableMapEffect(items) { map ->
+                if (clusterManager == null) {
+                    clusterManager = TedNaverClustering.with<NaverItem>(context, map)
+                        .customCluster {
+                            TextView(context).apply {
+                                text = "${it.size}개"
+                                background = AppCompatResources.getDrawable(
+                                    context,
+                                    R.drawable.circle_clustor
+                                )
+                                setPadding(150, 150, 150, 150)
+                            }
+                        }.customMarker {
+                            Marker().apply {
+                                icon = OverlayImage.fromResource(R.drawable.ic_nav_mypage_on)
+                                width = 60
+                                height = 86
+                            }
+                        }.make()
+                }
+                clusterManager?.addItems(items)
+                onDispose {
+                    clusterManager?.clearItems()
+                }
+            }
+//            Marker(
+//                state = MarkerState(position = LatLng()),
+//                captionText = "Marker in Seoul"
+//            )
+//            Marker(
+//                state = MarkerState(position = LatLng()),
+//                captionText = "Marker in Pangyo"
+//            )
+        }
     }
 }
+
