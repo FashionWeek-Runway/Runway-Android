cce4624742c7ce2ae0c048b2d253e029dc5de94b
PCBL <cksgodl1@naver.com>
2023-02-08 22:17:56 +0900

    feat : SignInViewModel 유저 정보등록 State 추출

--- app/src/main/java/com/cmc12th/runway/ui/signin/SignInViewModel.kt
+++ app/src/main/java/com/cmc12th/runway/ui/signin/SignInViewModel.kt
@@ -1,11 +1,65 @@
 package com.cmc12th.runway.ui.signin
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.cmc12th.runway.ui.signin.model.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class SignInViewModel @Inject constructor(
-
 ) : ViewModel() {
+
+    private val _nameAndNationality =
+        mutableStateOf<NameAndNationality>(NameAndNationality.default())
+    val nameAndNationality: State<NameAndNationality> = _nameAndNationality
+
+    private val _gender = mutableStateOf(Gender.Unknown)
+    val gender: State<Gender> = _gender
+
+    private val _birth = mutableStateOf(Birth.default())
+    val birth: State<Birth> = _birth
+
+    private val _phone = mutableStateOf(Phone.default())
+    val phone: State<Phone> = _phone
+
+    private val _userVerificationStatus = mutableStateOf(false)
+    val userVerificationStatus: State<Boolean> = _userVerificationStatus
+
+    fun updateName(name: String) {
+        _nameAndNationality.value = _nameAndNationality.value.copy(name = name)
+        checkUserVerificationStatus()
+    }
+
+    fun updateNationality(nationality: Nationality) {
+        _nameAndNationality.value = _nameAndNationality.value.copy(nationality = nationality)
+    }
+
+    fun updateGender(gender: Gender) {
+        _gender.value = gender
+        checkUserVerificationStatus()
+    }
+
+    fun updateBirth(birth: Birth) {
+        _birth.value = birth
+        checkUserVerificationStatus()
+    }
+
+    fun updateMobileCarrier(mobileCarrier: MobileCarrier) {
+        _phone.value = _phone.value.copy(mobileCarrier = mobileCarrier)
+    }
+
+    fun updatePhoneNumber(phoneNumber: String) {
+        _phone.value = _phone.value.copy(number = phoneNumber)
+        checkUserVerificationStatus()
+    }
+
+
+    private fun checkUserVerificationStatus() {
+        val isVerified =
+            _gender.value.isNotUnknown() && _nameAndNationality.value.name.isNotBlank() && _birth.value.checkValidation() && _phone.value.checkValidation()
+        _userVerificationStatus.value = isVerified
+    }
+
 }
