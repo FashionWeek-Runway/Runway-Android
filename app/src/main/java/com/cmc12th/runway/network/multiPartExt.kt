bd08f94f024b90f608f0447c110de4b601458c81
PCBL <cksgodl1@naver.com>
2023-02-11 03:17:29 +0900

    feat : MultipartBody 확장함수 정의

---
+++ app/src/main/java/com/cmc12th/runway/network/multiPartExt.kt
@@ -0,0 +1,7 @@
+package com.cmc12th.runway.network
+
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+
+/** MultipartBody으로 바꾸는 작업 */
+fun String.toPlainRequestBody() = requireNotNull(this).toRequestBody("text/plain".toMediaType())
