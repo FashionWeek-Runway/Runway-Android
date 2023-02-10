e53a36dcca772952fb6314900efc815a19bf696d
dlgocks1 <cksgodl1@naver.com>
2023-02-10 14:14:50 +0900

    feat : NIckname 모델 추가

---
+++ app/src/main/java/com/cmc12th/runway/ui/signin/model/Nickname.kt
@@ -0,0 +1,15 @@
+package com.cmc12th.runway.ui.signin.model
+
+data class Nickname(
+    val text: String,
+) {
+
+    fun checkValidate() = regex.matches(text)
+
+    companion object {
+        private val regex = Regex("""^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]{1,10}$""")
+        const val MAX_NICKNAME_LENGTH = 10
+
+        fun default() = Nickname("")
+    }
+}
