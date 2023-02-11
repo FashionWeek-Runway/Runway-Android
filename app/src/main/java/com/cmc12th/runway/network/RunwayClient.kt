f30e64379f64065fff1c8042402538745bb6d73c
PCBL <cksgodl1@naver.com>
2023-02-11 11:31:22 +0900

    feat : RunwayClinet에 API등록 및 typealias추가

--- app/src/main/java/com/cmc12th/runway/network/RunwayClient.kt
+++ app/src/main/java/com/cmc12th/runway/network/RunwayClient.kt
@@ -1,13 +1,49 @@
 package com.cmc12th.runway.network
 
-import com.cmc12th.runway.data.request.LoginRequest
+import com.cmc12th.runway.data.request.*
+import com.cmc12th.runway.data.response.LoginResponse
+import com.cmc12th.runway.data.response.SignUpResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.http.Body
+import retrofit2.http.Part
+import retrofit2.http.PartMap
+import retrofit2.http.Query
 import javax.inject.Inject
 
 class RunwayClient @Inject constructor(
     private val loginService: LoginService,
 ) {
-    /** Login */
+
     suspend fun login(loginRequest: LoginRequest) =
         loginService.login(loginRequest)
+
+    suspend fun sendVerifyMessage(sendVerifyMessageRequest: SendVerifyMessageRequest) =
+        loginService.sendVerifyMessage(sendVerifyMessageRequest)
+
+    suspend fun verifyPhoneNumber(loginCheckRequest: LoginCheckRequest) =
+        loginService.verifyPhoneNumber(loginCheckRequest)
+
+    suspend fun checkNickname(nickname: String) = loginService.checkNickname(nickname)
+
+    suspend fun duplicatedPhoneNumberCheck(phone: String) =
+        loginService.duplicatedPhoneNumberCheck(phone)
+
+    suspend fun kakaoLogin(oauthLoginRequest: OauthLoginRequest) =
+        loginService.kakaoLogin(oauthLoginRequest)
+
+    suspend fun modifyPassword(passwordAndPhoneNumberRequest: PasswordAndPhoneNumberRequest) =
+        loginService.modifyPassword(passwordAndPhoneNumberRequest)
+
+    suspend fun signUp(
+        feedPostReqeust: HashMap<String, RequestBody>,
+        categoryList: List<MultipartBody.Part>,
+    ) = loginService.signUp(feedPostReqeust, categoryList)
+
+    suspend fun kakoSignUp(
+        feedPostReqeust: HashMap<String, RequestBody>,
+        categoryList: List<MultipartBody.Part>,
+    ) = loginService.kakoSignUp(feedPostReqeust, categoryList)
+
 
 }
--- app/src/main/java/com/cmc12th/runway/utils/TypeAliases.kt
+++ app/src/main/java/com/cmc12th/runway/utils/TypeAliases.kt
@@ -1,7 +1,11 @@
 package com.cmc12th.runway.utils
 
 import com.cmc12th.runway.data.response.ResponseWrapper
+import com.cmc12th.runway.network.ApiState
 import retrofit2.Response
 
 typealias DefaultResponse = Response<ResponseWrapper<String>>
 typealias NetworkResponse<T> = Response<ResponseWrapper<T>>
+
+typealias ApiWrapper<T> = ApiState<ResponseWrapper<T>>
+typealias DefaultApiWrapper = ApiState<ResponseWrapper<String>>
