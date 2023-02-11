82cc77b2f186bc7bff2e6d8fe5d9776a1d3952e5
PCBL <cksgodl1@naver.com>
2023-02-11 20:36:39 +0900

    feat : FileExt 추가

---
+++ app/src/main/java/com/cmc12th/runway/utils/FileExt.kt
@@ -0,0 +1,47 @@
+package com.cmc12th.runway.utils
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
+import android.webkit.MimeTypeMap
+import java.io.*
+
+fun fileFromContentUri(context: Context, contentUri: Uri): File {
+    // Preparing Temp file name
+    val fileExtension = getFileExtension(context, contentUri)
+    val fileName = "temp_file" + if (fileExtension != null) ".$fileExtension" else ""
+
+    // Creating Temp file
+    val tempFile = File(context.cacheDir, fileName)
+    tempFile.createNewFile()
+
+    try {
+        val oStream = FileOutputStream(tempFile)
+        val inputStream = context.contentResolver.openInputStream(contentUri)
+
+        inputStream?.let {
+            copy(inputStream, oStream)
+        }
+
+        oStream.flush()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
+    return tempFile
+}
+
+private fun getFileExtension(context: Context, uri: Uri): String? {
+    val fileType: String? = context.contentResolver.getType(uri)
+    return MimeTypeMap.getSingleton().getExtensionFromMimeType(fileType)
+}
+
+@Throws(IOException::class)
+private fun copy(source: InputStream, target: OutputStream) {
+    val buf = ByteArray(8192)
+    var length: Int
+    while (source.read(buf).also { length = it } > 0) {
+        target.write(buf, 0, length)
+    }
+}
