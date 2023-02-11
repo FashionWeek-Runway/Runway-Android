072b7d89bb5d16a3385a7a1d6a51c9e3d51ffd80
PCBL <cksgodl1@naver.com>
2023-02-11 11:30:06 +0900

    refactor : 네트워크 요청은 백그라운드 코루틴에서 돌아가게끔

--- app/src/main/java/com/cmc12th/runway/network/safeFlow.kt
+++ app/src/main/java/com/cmc12th/runway/network/safeFlow.kt
@@ -4,8 +4,10 @@ import com.cmc12th.runway.data.response.ErrorResponse
 import com.cmc12th.runway.data.response.ResponseWrapper
 import com.cmc12th.runway.utils.GsonHelper
 import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import retrofit2.Response
 
 fun <T : Any> safeFlow(apiFunc: suspend () -> Response<ResponseWrapper<T>>): Flow<ApiState<ResponseWrapper<T>>> =
@@ -21,4 +23,4 @@ fun <T : Any> safeFlow(apiFunc: suspend () -> Response<ResponseWrapper<T>>): Flo
         } catch (e: Exception) {
             emit(ApiState.NotResponse(message = e.message ?: "", exception = e))
         }
-    }
+    }.flowOn(Dispatchers.IO)
