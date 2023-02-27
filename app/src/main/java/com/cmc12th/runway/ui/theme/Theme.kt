44c7e8ff532aa910e7481be24b66417e5aaf0b79
dlgocks1 <cksgodl1@naver.com>
2023-02-27 15:18:18 +0900

    fix : 다크테마 적용 X

--- app/src/main/java/com/cmc12th/runway/ui/theme/Theme.kt
+++ app/src/main/java/com/cmc12th/runway/ui/theme/Theme.kt
@@ -42,14 +42,16 @@ fun RunwayTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            if (darkTheme) dynamicLightColorScheme(context) else dynamicLightColorScheme(context)
         }
-        darkTheme -> DarkColorScheme
+//        darkTheme -> DarkColorScheme
+        darkTheme -> LightColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
