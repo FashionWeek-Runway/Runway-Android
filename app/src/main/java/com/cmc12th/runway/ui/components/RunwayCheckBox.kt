887dde538a2cd8dd1274d9f5424437db9fcffefa
dlgocks1 <cksgodl1@naver.com>
2023-03-02 11:06:40 +0900

    design : 체크박스 컴포저블 디자인 수정

--- app/src/main/java/com/cmc12th/runway/ui/components/RunwayCheckBox.kt
+++ app/src/main/java/com/cmc12th/runway/ui/components/RunwayCheckBox.kt
@@ -6,11 +6,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -27,9 +30,11 @@ fun RunwayCheckBox(
 ) {
     Box(
         modifier = Modifier
-            .size(24.dp)
+            .size(18.dp)
+            .clip(RoundedCornerShape(4.dp))
             .border(BorderStroke(if (checkState) 0.dp else 1.dp, Gray300))
             .background(if (checkState) Primary else Color.White)
+            .padding(3.dp)
             .clickable {
                 onChecked()
             }
