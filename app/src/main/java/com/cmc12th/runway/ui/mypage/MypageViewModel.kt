fa19b47a3b1efa80dae172bab52f5910e39cea03
PCBL <cksgodl1@naver.com>
2023-03-02 23:54:53 +0900

    feat : EditProfile API 기초 공사

--- app/src/main/java/com/cmc12th/runway/ui/mypage/MypageViewModel.kt
+++ app/src/main/java/com/cmc12th/runway/ui/mypage/MypageViewModel.kt
@@ -5,12 +5,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import com.cmc12th.runway.data.response.ErrorResponse
 import com.cmc12th.runway.data.response.user.MyReviewsItem
 import com.cmc12th.runway.data.response.user.StoreMetaDataItem
 import com.cmc12th.runway.domain.repository.AuthRepository
+import com.cmc12th.runway.domain.repository.SignInRepository
 import com.cmc12th.runway.ui.login.passwordsearch.SearchPasswordPhoneUiState
 import com.cmc12th.runway.ui.map.components.DetailState
 import com.cmc12th.runway.ui.mypage.view.MypageTabInfo
+import com.cmc12th.runway.ui.signin.SignInProfileImageUiState
+import com.cmc12th.runway.ui.signin.model.Nickname
+import com.cmc12th.runway.ui.signin.model.ProfileImageType
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
@@ -24,6 +29,7 @@ data class MypageUiState(
 @HiltViewModel
 class MypageViewModel @Inject constructor(
     private val authRepository: AuthRepository,
+    private val signInRepository: SignInRepository
 ) : ViewModel() {
 
     private val _myReviews = MutableStateFlow<PagingData<MyReviewsItem>>(PagingData.empty())
@@ -33,6 +39,18 @@ class MypageViewModel @Inject constructor(
     val bookmarkedStore: StateFlow<PagingData<StoreMetaDataItem>> = _bookmarkedStore.asStateFlow()
     private val _onDetail = MutableStateFlow(DetailState.default())
     private val _selectedPage = MutableStateFlow(MypageTabInfo.MY_REVIEW)
+
+    private val _nickName = MutableStateFlow(Nickname.default())
+    private val _profileImage = MutableStateFlow<ProfileImageType>(ProfileImageType.DEFAULT)
+
+    val profileImageUiState: StateFlow<SignInProfileImageUiState> =
+        combine(_profileImage, _nickName) { profileImage, nickName ->
+            SignInProfileImageUiState(profileImage = profileImage, nickName = nickName)
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = SignInProfileImageUiState()
+        )
 
     val uiState = combine(_onDetail, _selectedPage) { onDetail, selectedPage ->
         MypageUiState(
@@ -45,6 +63,15 @@ class MypageViewModel @Inject constructor(
         initialValue = MypageUiState()
     )
 
+    fun checkNickname(onSuccess: () -> Unit, onError: (ErrorResponse) -> Unit) =
+        viewModelScope.launch {
+            signInRepository.checkNickname(nickname = _nickName.value.text).collect {
+                it.onSuccess {
+                    onSuccess()
+                }
+                it.onError(onError)
+            }
+        }
 
     fun getMyReviews() = viewModelScope.launch {
         authRepository.myReviewPaging().cachedIn(viewModelScope).collect {
@@ -64,6 +91,27 @@ class MypageViewModel @Inject constructor(
 
     fun updateSelectedPage(tabInfo: MypageTabInfo) {
         _selectedPage.value = tabInfo
+    }
+
+    fun updateProfileImage(profileImage: ProfileImageType) {
+        _profileImage.value = profileImage
+    }
+
+    fun updateNickName(nickname: String) {
+        _nickName.value = _nickName.value.copy(text = nickname)
+    }
+
+    fun getMyProfile() = viewModelScope.launch {
+        authRepository.getProfileInfoToEdit().collect { apiState ->
+            apiState.onSuccess {
+                updateNickName(it.result.nickname)
+                if (it.result.imgUrl == null) {
+                    updateProfileImage(ProfileImageType.DEFAULT)
+                } else {
+                    updateProfileImage(ProfileImageType.SOCIAL(it.result.imgUrl))
+                }
+            }
+        }
     }
 
 }
