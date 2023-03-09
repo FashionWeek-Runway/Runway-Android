390a2d84eae7825dcf48dd137e2b0b00e98e0a44
dlgocks1 <cksgodl1@naver.com>
2023-03-09 11:02:13 +0900

    design : Detail 뷰 탑바 디자인 수정

--- app/src/main/java/com/cmc12th/runway/ui/detail/domain/ManageSystemBarColor.kt
+++ app/src/main/java/com/cmc12th/runway/ui/detail/domain/ManageSystemBarColor.kt
@@ -27,26 +27,27 @@ fun ManageSystemBarColor(
         }
     }
     LaunchedEffect(key1 = scrollState.value.dp) {
-        if (scrollState.value.dp < 100.dp) {
+        if (scrollState.value.dp < 200.dp) {
             systemUiController.setSystemBarsColor(topbarColor)
             systemUiController.setNavigationBarColor(color = Color.White)
         } else systemUiController.setSystemBarsColor(Color.White)
     }
 
     LaunchedEffect(key1 = scrollState.value.dp) {
-        if (scrollState.value.dp < 100.dp) {
-            if (scrollState.value.dp < 50.dp) {
-                updateTopbarColor(Color.Transparent)
-            } else {
-                updateTopbarColor(
-                    Color.White.copy(
-                        alpha = Float.min(
-                            (scrollState.value.dp) / 100.dp,
-                            100f
-                        )
-                    )
-                )
-            }
+        if (scrollState.value.dp < 200.dp) {
+//            if (scrollState.value.dp < 50.dp) {
+//                updateTopbarColor(Color.Transparent)
+//            } else {
+//                updateTopbarColor(
+//                    Color.White.copy(
+//                        alpha = Float.min(
+//                            (scrollState.value.dp) / 100.dp,
+//                            100f
+//                        )
+//                    )
+//                )
+//            }
+            updateTopbarColor(Color.Transparent)
             updateTopbarIconColor(Color.White)
         } else {
             updateTopbarColor(Color.White)
