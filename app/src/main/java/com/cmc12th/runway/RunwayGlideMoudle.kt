b55759ab32adc32c1277452348cd02d4c31aeaf0
dlgocks1 <cksgodl1@naver.com>
2023-03-21 15:06:25 +0900

    feat : 이미지 로드 및 캐싱관련 커스텀 Glide OkHttp 등록

---
+++ app/src/main/java/com/cmc12th/runway/RunwayGlideMoudle.kt
@@ -0,0 +1,62 @@
+package com.cmc12th.runway
+
+import android.content.Context
+import android.util.Log
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.Priority
+import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
+import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import java.io.InputStream
+import java.util.concurrent.TimeUnit
+
+
+@GlideModule
+class RunwayGlideMoudle : AppGlideModule() {
+
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+
+        // 이미지 불러오는 시간을 보고싶다면 아래 주석을 풀어주세요
+        // builder.setLogLevel(Log.VERBOSE)
+
+        builder.setDefaultRequestOptions(
+            RequestOptions()
+                .placeholder(R.color.gray100)
+                .encodeQuality(100) // 유저 리뷰 퀄리티는 50%로 설정
+                .dontTransform() // 이미지를 변형하지 않음
+                .dontAnimate() // 이미지를 애니메이션하지 않음
+                .format(DecodeFormat.PREFER_RGB_565) // 이미지 포맷을 RGB565로 설정
+                .priority(Priority.IMMEDIATE) // 이미지를 빠르게 불러옴
+                .diskCacheStrategy(DiskCacheStrategy.ALL) // 디스크 캐시 전략을 ALL로 설정
+                .error(R.drawable.img_dummy)    // 이미지 로드 실패시 더미 이미지를 보여줌
+        )
+    }
+
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+        val okHttpClient = OkHttpClient.Builder()
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS)
+            .writeTimeout(20, TimeUnit.SECONDS)
+            .retryOnConnectionFailure(true)
+            .build()
+
+        registry.replace(
+            GlideUrl::class.java,
+            InputStream::class.java,
+            OkHttpUrlLoader.Factory(okHttpClient)
+        )
+
+    }
+}
