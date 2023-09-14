4ecb6e82813b8df88f6a0377570f8c33247a67ae
PCBL <cksgodl1@naver.com>
2023-09-14 22:57:25 +0900

    Hotfix : 인스타 페이지 넘김 오류 수정 버전 수정

--- buildSrc/src/main/java/DefaultConfig.kt
+++ buildSrc/src/main/java/DefaultConfig.kt
@@ -2,8 +2,8 @@ object DefaultConfig {
     const val COMPILE_SDK_VERSION = 33
     const val MIN_SDK_VERSION = 21
     const val TARGET_SDK_VERSION = 33
-    const val VERSION_CODE = 9
-    const val VERSION_NAME = "1.17"
+    const val VERSION_CODE = 11
+    const val VERSION_NAME = "1.19"
     const val NAME_SAPCE = "com.cmc12th.runway"
     const val DATA_NAME_SAPCE = "com.cmc12th.runway.data"
     const val DOMAIN_NAME_SAPCE = "com.cmc12th.runway.domain"
